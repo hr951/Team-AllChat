b6b24b413c74ff6ed2c206a713ece3d315fa2351
hr951 <123540607+hr951@users.noreply.github.com>
2024-10-27 14:53:47 +0900

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,26 @@ import { world } from "@minecraft/server";
 
 world.beforeEvents.chatSend.subscribe((ev) => {
     const { message, sender } = ev;
+    const tags = [
+        { name: "A", teamname: "TeamNameA" },
+        { name: "B", teamname: "TeamNameB" },
+        { name: "C", teamname: "TeamNameC" },
+        { name: "D", teamname: "TeamNameD" }
+    ];
 
     if (message.startsWith("!") || message.startsWith("！")) {
         ev.cancel = true;
-
+            for (const tag of tags){
+                if(sender.hasTag("A") || sender.hasTag("B") || sender.hasTag("C") || sender.hasTag("D")){
+                if (!sender.hasTag(tag.name)) continue;
             const args = message.slice(1).split(" ");
-                world.sendMessage(`[ALL] §r<${sender.name}> ${args[0]}`)
+                world.sendMessage(`[${tag.teamname}] §r<${sender.name}> ${args[0]}`)
+            } else {
+                const args = message.slice(1).split(" ");
+                world.sendMessage(`[NoTag] §r<${sender.name}> ${args[0]}`)
+            }
                 return;
+            }
     } else {
     for (const player of world.getPlayers()) {
         ev.cancel = true;
